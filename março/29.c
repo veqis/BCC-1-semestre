cfa988b20134aac70b42d8a3ce0df36879b85fe7
veqis <61508945+veqis@users.noreply.github.com>
2022-03-29 15:55:15 -0300

    Update 29.c

--- março/29.c
+++ março/29.c
@@ -2,15 +2,30 @@
 
 int main(int argc, char const *argv[])
 {
-    int numero=0, fatorial=0;
+    int numero=0; 
+    double fatorial=0;
+
     printf("digite o numero: ");
     scanf("%i", &numero);
+    
+    if (numero<0)
+    {
+        printf("não possivel calcular\n");
+        return 0;
+    }
+    if (numero==0 || numero==1)
+    {
+        printf("o fatorial e: 1");
+        return 0;
+    }
+       
+
     fatorial=numero;
     for (int i = 1; i < numero-1; i++)
     {
         fatorial=fatorial*(numero-i);
     }
-    printf("o fatorial e: %i", fatorial);
+    printf("o fatorial e: %.2f", fatorial);
 
     return 0;
 }
